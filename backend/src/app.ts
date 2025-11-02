e1bded487d927feffba739e0dd6c4ba38a36dcc5
prathamps <pspratham16@gmail.com>
2025-11-02 10:26:25 +0530

    feat: Add envirnoment variables for frontend and backend and resolve cors issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,12 @@ import sweetRoutes from "./routes/sweetRoutes"
 
 const app = express()
 
-app.use(cors())
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL || "http://localhost:5173",
+		credentials: true,
+	})
+)
 app.use(express.json())
 
 app.use("/api/auth", authRoutes)
